95da1ede9861a4ea2c54babe570121976cc1384b
johnkirtley <kirtleyj16@gmail.com>
2020-01-17 08:23:22 -0800

    Finished Header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,27 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    const header = document.createElement('div'),
+        date = document.createElement('span'),
+        title = document.createElement('h1'),
+        temperature = document.createElement('span')
+
+    header.classList.add('header');
+    date.classList.add('date');
+    temperature.classList.add('temp');
+
+    date.textContent = 'March 28, 2019';
+    title.textContent = 'Lambda Times';
+    temperature.textContent = '98°';
+
+    header.append(date);
+    header.append(title);
+    header.append(temperature);
+
+    return header;
+}
+
+const headerContainer = document.querySelector('.header-container');
+
+headerContainer.append(Header());
